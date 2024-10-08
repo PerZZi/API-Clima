480e4a8ddfc72756cb3a30a847c5c1bbb3293c04
PerZZi <emiliano.estebandiaz@gmail.com>
2024-10-08 17:46:23 -0300

    Subida de api con limit,cache y Jwt

--- src/main/java/com/example/ApiClima/ApiClimaApplication.java
+++ src/main/java/com/example/ApiClima/ApiClimaApplication.java
@@ -2,8 +2,11 @@ package com.example.ApiClima;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableCaching
 public class ApiClimaApplication {
 
 	public static void main(String[] args) {
